9c269cee3727e1f7fbd2e94f01f7e3a0dd2f526d
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-12-06 20:19:03 +0300

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Header, Welcome } from "./components";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Welcome />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__reserved">
+        &#169; 2022 Taskify &#8482;. All rights reserved{" "}
+      </div>
+      <div className="footer__spacer"></div>
+      <div className="footer__items">About</div>
+      <div className="footer__items">Privacy Policy</div>
+      <div className="footer__items">Licensing</div>
+    </footer>
+  );
+}
