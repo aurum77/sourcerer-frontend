51728e7c8f5e2ee24bf2442cc544af98bc88bda0
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-12-11 09:05:33 +0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const navlinks = [
+  {
+    id: "home",
+    title: "Home"
+  },
+  {
+    id: "features",
+    title: "Features"
+  },
+  {
+    id: "signin",
+    title: "Sign In"
+  },
+  {
+    id: "signup",
+    title: "Sign Up"
+  },
+]
