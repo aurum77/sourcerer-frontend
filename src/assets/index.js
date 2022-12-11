c253689b99b132f86591be03c27bd1dd5f175f2f
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-12-11 10:44:49 +0300

    move and add some assets

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import mars from "./mars.png";
+import close from "./close.svg";
+import menu from "./menu.svg";
+import react from "./react.svg";
+import vite from "./vite.svg";
+
+export {
+  mars,
+  close,
+  menu,
+  react,
+  vite
+};
