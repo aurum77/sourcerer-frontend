eb636d386e58c577c72748ad6843a6ba4a435b03
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-12-12 23:36:36 +0300

    change motto and make hero more responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,10 @@ const Hero = () => {
   return (
     <div className="hero__container">
       <div className="hero__msg">
-        <div>Let's</div>
-        <div>build</div>
-        <div>a</div>
-        <div>heckin</div>
-        <div>website</div>
+        <div>Easy</div>
+        <div>Tasks?</div>
+        <div>That's</div>
+        <div>Sourcerer</div>
       </div>
       <img src={mars} className="hero__img" />
     </div>
