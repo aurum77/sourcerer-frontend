d24cf32d868392263ca54f9846ed9d39b118ff69
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-12-13 17:01:26 +0300

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HeroPage } from "./pages";
+import { HeroPage, LoginPage, NotFoundPage } from "./pages";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="*" element=<NotFoundPage /> />
         <Route path="/" element=<HeroPage /> />
+        <Route path="/login" element=<LoginPage /> />
       </Routes>
     </Router>
   );
